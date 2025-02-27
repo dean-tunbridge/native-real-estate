dba8c7d7ca98e5abccdb4024b00835a16d6f9f75
Dean Tunbridge <deantunbridgedev@gmail.com>
2025-02-27 16:45:30 +1300

    try and fix errors with setup of tailwind

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -9,6 +9,8 @@ export default function Index() {
         justifyContent: 'center',
         alignItems: 'center',
       }}>
+        <Text className="font-bold text-lg my-10">Welcome to Real Estate</Text>
+
       <Link href="/sign-in">Sign In</Link>
       <Link href="/explore">Explore</Link>
       <Link href="/profile">Profile</Link>
