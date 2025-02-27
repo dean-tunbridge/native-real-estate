1aa83e0980ce6209b86bbdc0e516c4cfab7f9b0b
Dean Tunbridge <deantunbridgedev@gmail.com>
2025-02-27 16:22:15 +1300

    begin work on index . Add Links to each page

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,15 +1,18 @@
-import { Text, View } from "react-native";
+import { Link } from 'expo-router'
+import { Text, View } from 'react-native'
 
 export default function Index() {
   return (
     <View
       style={{
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+      <Link href="/sign-in">Sign In</Link>
+      <Link href="/explore">Explore</Link>
+      <Link href="/profile">Profile</Link>
+      <Link href="/properties/1">Property</Link>
     </View>
-  );
+  )
 }
