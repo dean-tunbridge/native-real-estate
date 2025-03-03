0bdd3f1d867f453a9f2cff4f01d368205b8e79e6
Dean Tunbridge <deantunbridgedev@gmail.com>
2025-03-03 13:36:29 +1300

    use scroll view for mobile scrolling

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -1,10 +1,10 @@
-import { View, Text, SafeAreaView } from 'react-native'
+import { View, Text, SafeAreaView, ScrollView } from 'react-native'
 import React from 'react'
 
 const SignIn = () => {
   return (
-    <SafeAreaView>
-      <Text>sign-in</Text>
+    <SafeAreaView className="bg-white h-full">
+      <ScrollView contentContainerClassName="h-full"></ScrollView>
     </SafeAreaView>
   )
 }
