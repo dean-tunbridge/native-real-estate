54b5e2291e774f793baf23e2e11ac93478386210
Dean Tunbridge <deantunbridgedev@gmail.com>
2025-03-03 13:10:44 +1300

    add useEffect to check if lfonts are loading

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,7 @@
-import { Stack } from 'expo-router'
+import { SplashScreen, Stack } from 'expo-router'
 import './global.css'
 import { useFonts } from 'expo-font'
+import { useEffect } from 'react'
 
 export default function RootLayout() {
   const [fontsLoaded] = useFonts({
@@ -11,6 +12,16 @@ export default function RootLayout() {
     'Rubik-Regular': require('../assets/fonts/Rubik-Regular.ttf'),
     'Rubik-SemiBold': require('../assets/fonts/Rubik-SemiBold.ttf'),
   })
+
+  useEffect(() => {
+    if (fontsLoaded) {
+      SplashScreen.hideAsync()
+    }
+  }, [fontsLoaded])
+
+  if (!fontsLoaded) {
+    return null
+  }
 
   return <Stack />
 }
