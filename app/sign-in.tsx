7b8da4b8baede5b7e3b992218377ec3f8a8be86b
Dean Tunbridge <deantunbridgedev@gmail.com>
2025-03-03 13:38:48 +1300

    had issue with Image tag and auto import. Have fixed and now mage tag is working

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -1,10 +1,17 @@
-import { View, Text, SafeAreaView, ScrollView } from 'react-native'
+import { View, Text, SafeAreaView, ScrollView, Image } from 'react-native'
 import React from 'react'
+import images from '@/constants/images'
 
 const SignIn = () => {
   return (
     <SafeAreaView className="bg-white h-full">
-      <ScrollView contentContainerClassName="h-full"></ScrollView>
+      <ScrollView contentContainerClassName="h-full">
+        <Image
+          source={images.onboarding}
+          className="w-full h-4/6"
+          resizeMode="contain"
+        />
+      </ScrollView>
     </SafeAreaView>
   )
 }
