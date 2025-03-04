ca776a4e85dff1a21204541f27a29b2352ebf077
Dean Tunbridge <deantunbridgedev@gmail.com>
2025-03-04 15:16:59 +1300

    improve readability of code by adding spaces

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -26,16 +26,19 @@ const SignIn = () => {
           <Text className="text-base text-center uppercase font-rubik text-black-200">
             Welcome to Real Estate
           </Text>
+
           <Text className="text-3xl font-rubik-bold text-black-300 text-center mt-2">
             Let's get you closer to {'\n'}
             <Text className="text-primary-300">Your ideal home</Text>
           </Text>
+
           <Text className="text-lg font-rubik text-black-200 text-center mt-12">
             Login to Real Estate with google
           </Text>
+
           <TouchableOpacity
             onPress={handleLogin}
-            className="bg-white shadow-md shadow-zinc-300 rounded-full w-full">
+            className="bg-white shadow-md shadow-zinc-300 rounded-full w-full py-4 mt-5">
             <View className="flex flex-row items-center justify-center">
               <Image
                 source={icons.google}
