d1cbc92c3dc34ea41cd87d87e14909e1a8a56b2d
Dean Tunbridge <deantunbridgedev@gmail.com>
2025-03-06 15:39:41 +1300

    building rest  of login. Added lobal-rpovider file to remove erreor but still getting it

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -5,15 +5,27 @@ import {
   ScrollView,
   Image,
   TouchableOpacity,
+  Alert,
 } from 'react-native'
 import React from 'react'
 import images from '@/constants/images'
 import icons from '@/constants/icons'
 import { login } from '@/lib/appwrite'
+import { useGlobalContext } from '@/lib/global-provider'
+import { Redirect } from 'expo-router'
 
 const SignIn = () => {
+  const { refetch, loading, isLoggedIn } = useGlobalContext()
+
+  if (!loading && isLoggedIn) return <Redirect href="/" />
+
   const handleLogin = async () => {
     const result = await login()
+    if (result) {
+      refetch()
+    } else {
+      Alert.alert('Error', 'Failed to login')
+    }
   }
 
   return (
