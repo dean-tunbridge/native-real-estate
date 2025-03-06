67e4042705d7eb4c200a1d1cc8fa63d941b18b9c
Dean Tunbridge <deantunbridgedev@gmail.com>
2025-03-06 15:55:34 +1300

    beginwork on profile. Haviung issues with auto imports on this project

--- app/(root)/(tabs)/profile.tsx
+++ app/(root)/(tabs)/profile.tsx
@@ -1,10 +1,21 @@
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView, Image } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import icons from '@/constants/icons'
 
 const Profile = () => {
+  const handleLogout = async () => {}
+
   return (
-    <View>
-      <Text>Profile</Text>
-    </View>
+    <SafeAreaView className="h-full bg-white">
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerClassName="pb-32 px-7">
+        <View>
+          <Text className="text-xl font-rubik-bold">Profile</Text>
+          <Image source={icons.bell} className="size-5" />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   )
 }
 
