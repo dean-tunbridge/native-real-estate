2a350e90f39229a069bdaa0cb874fb58cb2b9bdd
Dean Tunbridge <deantunbridgedev@gmail.com>
2025-03-06 16:06:34 +1300

    create function and interface for settingsItem

--- app/(root)/(tabs)/profile.tsx
+++ app/(root)/(tabs)/profile.tsx
@@ -3,6 +3,35 @@ import { SafeAreaView } from 'react-native-safe-area-context'
 import icons from '@/constants/icons'
 import images from '@/constants/images'
 
+interface SettingsItemProp {
+  icon: ImageSourcePropType
+  title: string
+  onPress?: () => void
+  textStyle?: string
+  showArrow?: boolean
+}
+
+const SettingsItem = ({
+  icon,
+  title,
+  onPress,
+  textStyle,
+  showArrow = true,
+}: SettingsItemProp) => (
+  <TouchableOpacity
+    onPress={onPress}
+    className="flex flex-row items-center justify-between py-3">
+    <View className="flex flex-row items-center gap-3">
+      <Image source={icon} className="size-6" />
+      <Text className={`text-lg font-rubik-medium text-black-300 ${textStyle}`}>
+        {title}
+      </Text>
+    </View>
+
+    {showArrow && <Image source={icons.rightArrow} className="size-5" />}
+  </TouchableOpacity>
+)
+
 const Profile = () => {
   const handleLogout = async () => {}
 
