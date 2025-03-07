08a31e3e2d1402ae26ad0861084a635586e52564
Dean Tunbridge <deantunbridgedev@gmail.com>
2025-03-07 19:04:29 +1300

    begin work on home page

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,17 +1,17 @@
+import images from '@/constants/images'
 import { Link } from 'expo-router'
-import { Text, View } from 'react-native'
+import { Text, View, Image } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}>
-      <Text className="font-bold text-3xl my-10 font-rubik">
-        Welcome to Real Estate
-      </Text>
-    </View>
+    <SafeAreaView>
+      <Text className="bg-white h-full">Home</Text>
+      <View className="px-5">
+        <View className="flex flex-row items-center">
+          <Image source={images.avatar} />
+        </View>
+      </View>
+    </SafeAreaView>
   )
 }
