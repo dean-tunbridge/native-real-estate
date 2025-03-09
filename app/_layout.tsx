570435100843032dfebbc2f9739d989e2161045c
Dean Tunbridge <deantunbridgedev@gmail.com>
2025-03-10 11:31:06 +1300

    update layout to try and fix app breaking error with global provider

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,10 @@
-import { SplashScreen, Stack } from 'expo-router'
-import './global.css'
-import { useFonts } from 'expo-font'
 import { useEffect } from 'react'
+import { Stack } from 'expo-router'
+import { useFonts } from 'expo-font'
+import * as SplashScreen from 'expo-splash-screen'
+
+import './global.css'
+import GlobalProvider from '@/lib/global-provider'
 
 export default function RootLayout() {
   const [fontsLoaded] = useFonts({
@@ -23,5 +26,9 @@ export default function RootLayout() {
     return null
   }
 
-  return <Stack screenOptions={{headerShown: false}}/>
+  return (
+    <GlobalProvider>
+      <Stack screenOptions={{ headerShown: false }} />
+    </GlobalProvider>
+  )
 }
